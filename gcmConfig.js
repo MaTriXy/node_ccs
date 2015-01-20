cd2d953f6f30cfdb8cec61b7bc80ec1326701700
mseshachalam <seshachalam@outlook.com>
2015-01-20 14:20:24 +0530

    till now

--- gcmConfig.js
+++ gcmConfig.js
@@ -1,6 +1,6 @@
 var config = {};
-config.jid = '1026645507924@gcm.googleapis.com';
-config.password = 'AIzaSyDxf8TTgcl4KLHv-4HsEp8nUmnjENMTt6A';
+config.jid = 'PROJECT_ID@gcm.googleapis.com';
+config.password = 'API_KEY';
 config.port = 5235;
 config.host = 'gcm.googleapis.com';
 
